c71fd962f5e4b9ec6ff65cf6db520ef4bdaa4320
kserin <kserin@gmail.com>
2016-03-29 15:07:22 +0300

    add loadSamples method

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -26,6 +26,12 @@ var App = React.createClass({
     this.setState({ fishes : this.state.fishes });
   },
 
+  loadSamples: function(){
+    this.setState({
+          fishes: require('./sample-fishes')
+    });
+  },
+
   render: function(){
     return (
         <div className="catch-of-the-day">
@@ -33,7 +39,7 @@ var App = React.createClass({
             <Header tagline="Fresh Seafood Market"/>
           </div>
           <Order />
-          <Inventory addFish={this.addFish}/>
+          <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
         </div>
     );
   }
@@ -105,6 +111,7 @@ var Inventory = React.createClass({
       <div>
         <h2>Inventory</h2>
         <AddFishForm {...this.props}/>
+        <button onClick={this.props.loadSamples}> Load Sample Fishes </button>
       </div>
     )
   }
