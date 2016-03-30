5c5eb5230ec11b57914f81ef15cd4b717323670c
kserin <kserin@gmail.com>
2016-03-30 15:57:29 +0300

    impl. continued

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -134,6 +134,22 @@ var Header = React.createClass({
 });
 
 var Order = React.createClass({
+  renderOrder : function(key) {
+   var fish = this.props.fishes[key];
+   var count = this.props.order[key];
+
+   if(!fish) {
+     return <li key={key}>Sorry, fish no longer available!</li>
+   }
+
+   return (
+     <li>
+       {count}lbs
+       {fish.name}
+       <span className="price">{h.formatPrice(count * fish.price)}</span>
+     </li>)
+ },
+
   render : function() {
 
     var orderIds = Object.keys(this.props.order);
@@ -153,9 +169,10 @@ var Order = React.createClass({
       <div className="order-wrap">
           <h2 className="order-title"> Your Order </h2>
           <ul className="order">
+              {orderIds.map(this.renderOrder)}
               <li className="total">
                   <strong> Total: </strong>
-                  {total}
+                  {h.formatPrice(total)}
               </li>
           </ul>
       </div>
