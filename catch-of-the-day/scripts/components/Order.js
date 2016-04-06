cccc02817989393089ad53ad6a0bdc3d5ce6cf64
kserin <kserin@gmail.com>
2016-04-06 14:05:36 +0300

    es6 modules

---
+++ catch-of-the-day/scripts/components/Order.js
@@ -0,0 +1,62 @@
+/*
+  Order
+  <Order/>
+*/
+var React = require('react');
+import CSSTransitionsGroup from 'react-addons-css-transition-group';
+import h from '../helpers';
+
+var Order = React.createClass({
+  renderOrder : function(key) {
+    var fish = this.props.fishes[key];
+    var count = this.props.order[key];
+    var removeButton = <button onClick={this.props.removeFromOrder.bind(null, key)}> &times </button>;
+
+    if(!fish) {
+      return <li key={key}>Sorry, fish no longer available! {removeButton}</li>
+    }
+
+    return (
+      <li key={key}>
+        {count}lbs
+        {fish.name}
+        <span className="price">{h.formatPrice(count * fish.price)}</span>
+        {removeButton}
+      </li>)
+  },
+  render : function() {
+    var orderIds = Object.keys(this.props.order);
+
+    var total = orderIds.reduce((prevTotal, key)=> {
+      var fish = this.props.fishes[key];
+      var count = this.props.order[key];
+      var isAvailable = fish && fish.status === 'available';
+
+      if(fish && isAvailable) {
+        return prevTotal + (count * parseInt(fish.price) || 0);
+      }
+
+      return prevTotal;
+    }, 0);
+
+    return (
+      <div className="order-wrap">
+        <h2 className="order-title">Your Order</h2>
+        <ul className="order">
+          {orderIds.map(this.renderOrder)}
+          <li className="total">
+            <strong>Total:</strong>
+            {h.formatPrice(total)}
+          </li>
+        </ul>
+      </div>
+    )
+  },
+  propTypes: {
+    order: React.PropTypes.object.isRequired,
+    removeFromOrder:React.PropTypes.func.isRequired,
+    fishes:React.PropTypes.object.isRequired
+  }
+})
+
+export default Order;
--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -23,6 +23,8 @@ import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker';
 import Header from './components/Header';
 import Fish from './components/Fish';
+import Order from './components/Order';
+
 
 /*
   App
@@ -142,63 +144,6 @@ var AddFishForm = React.createClass({
     )
   }
 });
-
-/*
-  Order
-  <Order/>
-*/
-var Order = React.createClass({
-  renderOrder : function(key) {
-    var fish = this.props.fishes[key];
-    var count = this.props.order[key];
-    var removeButton = <button onClick={this.props.removeFromOrder.bind(null, key)}> &times </button>;
-
-    if(!fish) {
-      return <li key={key}>Sorry, fish no longer available! {removeButton}</li>
-    }
-
-    return (
-      <li key={key}>
-        {count}lbs
-        {fish.name}
-        <span className="price">{h.formatPrice(count * fish.price)}</span>
-        {removeButton}
-      </li>)
-  },
-  render : function() {
-    var orderIds = Object.keys(this.props.order);
-
-    var total = orderIds.reduce((prevTotal, key)=> {
-      var fish = this.props.fishes[key];
-      var count = this.props.order[key];
-      var isAvailable = fish && fish.status === 'available';
-
-      if(fish && isAvailable) {
-        return prevTotal + (count * parseInt(fish.price) || 0);
-      }
-
-      return prevTotal;
-    }, 0);
-
-    return (
-      <div className="order-wrap">
-        <h2 className="order-title">Your Order</h2>
-        <ul className="order">
-          {orderIds.map(this.renderOrder)}
-          <li className="total">
-            <strong>Total:</strong>
-            {h.formatPrice(total)}
-          </li>
-        </ul>
-      </div>
-    )
-  },
-  propTypes: {
-    order: React.PropTypes.object.isRequired,
-    removeFromOrder:React.PropTypes.func.isRequired,
-    fishes:React.PropTypes.object.isRequired
-  }
-})
 
 /*
   Inventory
