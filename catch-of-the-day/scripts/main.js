2a53578e6cd1ccf95aee6912cf1961ed7825cc60
kserin <kserin@gmail.com>
2016-04-06 10:02:05 +0300

    add validation with proptypes

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -180,6 +180,9 @@ var Header = React.createClass({
         <h3 className="tagline"><span>{this.props.tagline}</span></h3>
       </header>
     )
+  },
+  propTypes: {
+    tagline: React.PropTypes.string.isRequired
   }
 })
 
@@ -232,6 +235,11 @@ var Order = React.createClass({
         </ul>
       </div>
     )
+  },
+  propTypes: {
+    order: React.PropTypes.object.isRequired,
+    removeFromOrder:React.PropTypes.func.isRequired,
+    fishes:React.PropTypes.object.isRequired
   }
 })
 
@@ -269,6 +277,13 @@ var Inventory = React.createClass({
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
+  },
+  propTypes: {
+    addFish: React.PropTypes.func.isRequired,
+    loadSamples:React.PropTypes.func.isRequired,
+    fishes:React.PropTypes.object.isRequired,
+    linkState:React.PropTypes.func.isRequired,
+    removeFish:React.PropTypes.func.isRequired
   }
 })
 
