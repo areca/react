9a808320d30dfa30a3083720351f375d0c83ac96
kserin <kserin@gmail.com>
2016-04-06 14:12:51 +0300

    es6 modules

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -24,6 +24,7 @@ import StorePicker from './components/StorePicker';
 import Header from './components/Header';
 import Fish from './components/Fish';
 import Order from './components/Order';
+import Inventory from './components/Inventory';
 
 
 /*
@@ -105,89 +106,6 @@ var App = React.createClass({
     )
   }
 });
-
-/*
-  Add Fish Form
-  <AddFishForm />
-*/
-
-var AddFishForm = React.createClass({
-  createFish : function(event) {
-    // 1. Stop the form from submitting
-    event.preventDefault();
-    // 2. Take the data from the form and create an object
-    var fish = {
-      name : this.refs.name.value,
-      price : this.refs.price.value,
-      status : this.refs.status.value,
-      desc : this.refs.desc.value,
-      image : this.refs.image.value
-    }
-
-    // 3. Add the fish to the App State
-    this.props.addFish(fish);
-    this.refs.fishForm.reset();
-  },
-  render : function() {
-    return (
-      <form className="fish-edit" ref="fishForm" onSubmit={this.createFish}>
-        <input type="text" ref="name" placeholder="Fish Name"/>
-        <input type="text" ref="price" placeholder="Fish Price" />
-        <select ref="status">
-          <option value="available">Fresh!</option>
-          <option value="unavailable">Sold Out!</option>
-        </select>
-        <textarea type="text" ref="desc" placeholder="Desc"></textarea>
-        <input type="text" ref="image" placeholder="URL to Image" />
-        <button type="submit">+ Add Item </button>
-      </form>
-    )
-  }
-});
-
-/*
-  Inventory
-  <Inventory/>
-*/
-var Inventory = React.createClass({
-  renderInventory : function(key) {
-    var linkState = this.props.linkState;
-    return (
-      <div className="fish-edit" key={key}>
-        <input type="text" valueLink={linkState('fishes.'+ key +'.name')}/>
-        <input type="text" valueLink={linkState('fishes.'+ key +'.price')}/>
-        <select valueLink={linkState('fishes.' + key + '.status')}>
-          <option value="unavailable">Sold Out!</option>
-          <option value="available">Fresh!</option>
-        </select>
-
-        <textarea valueLink={linkState('fishes.' + key + '.desc')}></textarea>
-        <input type="text" valueLink={linkState('fishes.'+ key +'.image')}/>
-        <button onClick={this.props.removeFish.bind(null, key)}>Remove Fish</button>
-
-      </div>
-    )
-  },
-  render : function() {
-    return (
-      <div>
-        <h2>Inventory</h2>
-
-        {Object.keys(this.props.fishes).map(this.renderInventory)}
-
-        <AddFishForm {...this.props} />
-        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
-      </div>
-    )
-  },
-  propTypes: {
-    addFish: React.PropTypes.func.isRequired,
-    loadSamples:React.PropTypes.func.isRequired,
-    fishes:React.PropTypes.object.isRequired,
-    linkState:React.PropTypes.func.isRequired,
-    removeFish:React.PropTypes.func.isRequired
-  }
-})
 
 /*
   Routes
