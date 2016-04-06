c8f81eb119822a4507c7bc636388e58ff20b5ead
kserin <kserin@gmail.com>
2016-04-06 13:52:45 +0300

    es6 modules

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -20,6 +20,7 @@ var Catalyst = require('react-catalyst');
 */
 
 import NotFound from './components/NotFound';
+import StorePicker from './components/StorePicker';
 
 /*
   App
@@ -292,32 +293,6 @@ var Inventory = React.createClass({
     removeFish:React.PropTypes.func.isRequired
   }
 })
-
-
-/*
-  StorePicker
-  This will let us make <StorePicker/>
-*/
-
-var StorePicker = React.createClass({
-  mixins : [History],
-  goToStore : function(event) {
-    event.preventDefault();
-    // get the data from the input
-    var storeId = this.refs.storeId.value;
-    this.history.pushState(null, '/store/' + storeId);
-  },
-  render : function() {
-    return (
-      <form className="store-selector" onSubmit={this.goToStore}>
-        <h2>Please Enter A Store</h2>
-        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
-        <input type="Submit" />
-      </form>
-    )
-  }
-
-});
 
 /*
   Routes
