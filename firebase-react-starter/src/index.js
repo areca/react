0913c44262297862851826ef8703f68fc28f81c9
kserin <kserin@gmail.com>
2016-09-16 17:31:52 +0300

    add firebase

--- firebase-react-starter/src/index.js
+++ firebase-react-starter/src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyAK6GZq9pA1qVKQP5iV9RlmwhJoBbyMspI",
+    authDomain: "fir-react-starter-e0b2d.firebaseapp.com",
+    databaseURL: "https://fir-react-starter-e0b2d.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "243695225105"
+  };
+
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
