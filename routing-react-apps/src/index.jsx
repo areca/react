1733e7d2f09fa1af354aba353848b5d67f70ad79
kserin <kserin@gmail.com>
2016-09-23 10:48:10 +0300

    impl

---
+++ routing-react-apps/src/index.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+// Import routing components
+import {Router, Route} from 'react-router';
+
+class Home extends Component {
+    render(){
+        return (<h1>Home Page</h1>);
+    }
+}
+
+// More components
+class Car extends Component {
+    render(){
+        return (<h1>Cars page</h1>);
+    }
+}
+
+class About extends Component {
+    render(){
+        return (<h1>About page</h1>);
+    }
+}
+
+render(
+    <Router>
+        <Route path="/" component={Home}/>
+        <Route path="/cars" component={Car}/>
+        <Route path="/about" component={About}/>
+    </Router>,
+    document.getElementById('container')
+);
